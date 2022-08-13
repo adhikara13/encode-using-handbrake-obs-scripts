5cb5059a863cbc9e59edfc6ba54bfdd29ff18cb5
ivanamin <ivanamin@student.21-school.ru>
2022-08-14 00:42:54 +0300

    Added option to use built-in preset from HandBrakeCLI & Fixed bug OBS freezes after recording

--- obs_to_handbrake.py
+++ obs_to_handbrake.py
@@ -3,10 +3,13 @@ import datetime
 import pathlib
 import subprocess
 
+presets = ["Very Fast 1080p30", "Very Fast 720p30"]
+
 class Data:
     Handbrake = None
     OutputDir = None
     OutputConvert = None
+    PresetList = None
 
 def frontend_event_handler(data):
     if data == obs.OBS_FRONTEND_EVENT_RECORDING_STOPPED:
@@ -25,22 +28,30 @@ def frontend_event_handler(data):
                 convert = pathlib.Path(Data.OutputConvert)
                 namefile = pathlib.Path(thefile).stem
                 subprocess.run(fr'explorer /select,{thefile}')
-  
-                subprocess.run(fr'{cli} --preset "Very Fast 720p30" -i "{thefile}" -o "{convert}\{namefile}.m4v"')
-                
+                cmd = fr'{cli} --preset "{Data.PresetList}" -i "{thefile}" -o "{convert}\{namefile}.m4v"'
+                subprocess.Popen(["powershell", "-Command", cmd])
                 
         return True
+
+def script_load(settings):
+    Data.Handbrake = obs.obs_data_get_string(settings,"handbrake")
+    Data.OutputDir = obs.obs_data_get_string(settings,"outputdir")
+    Data.OutputConvert = obs.obs_data_get_string(settings,"outputconvert")
+    Data.PresetList = obs.obs_data_get_string(settings,"presetlist")
 
 def script_update(settings):
     Data.Handbrake = obs.obs_data_get_string(settings,"handbrake")
     Data.OutputDir = obs.obs_data_get_string(settings,"outputdir")
     Data.OutputConvert = obs.obs_data_get_string(settings,"outputconvert")
+    Data.PresetList = obs.obs_data_get_string(settings,"presetlist")
+    
 
 def script_description():
-    return ("Choose the HandbrakeCLI.exe file and folder where recordings are saved."
+    return ('<center><h2> OBS to HandBrakeCLI </h2></center><center><h4>If you run into any problems, open an issue on <a href="https://github.com/adhikara13/encode-using-handbrake-obs-scripts">GitHub</a></h4></center>' 
+        "Choose the HandbrakeCLI.exe file and folder where recordings are saved."
         " When recording is stopped, the most-recent file"
-            " will be converted and shown selected in Windows Explorer."
-            " \n\nBy: adhkr (credit to: dustractor)")
+        " will be converted and shown selected in Windows Explorer."
+        "<h6>By: adhkr (credit to: dustractor) </h6>")
 
 def script_properties():
     props = obs.obs_properties_create()
@@ -53,6 +64,11 @@ def script_properties():
     obs.obs_properties_add_path(
         props, "outputconvert", "Output folder", obs.OBS_PATH_DIRECTORY,
         None, str(pathlib.Path.home()))
+    preset_list = obs.obs_properties_add_list(
+        props, "presetlist", "Presets", obs.OBS_COMBO_TYPE_LIST,
+        obs.OBS_COMBO_FORMAT_STRING)
+    for all in presets: 
+        obs.obs_property_list_add_string(preset_list, all, all)
     return props
 
 obs.obs_frontend_add_event_callback(frontend_event_handler)
